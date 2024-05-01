1304f2e2319869b1187a1ef24cab34c841e2b8fe
Vaidik Nakrani <134259164+vaidikcs@users.noreply.github.com>
2024-05-02 00:17:43 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -55,6 +55,6 @@ class Predictor(BasePredictor):
         else:
             inv_depth = self.large_model(image)
 
-        inv_depth = inv_depth.cpu().numpy().tobytes()
+        inv_depth = inv_depth.cpu().detach().numpy().tobytes()
 
         return Path(inv_depth)
